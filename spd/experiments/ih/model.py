04abb01654b1c1c451e72a6aa8b1ebe13cd0f19a
Casper Lützhøft Christensen <61698286+casperlchristensen@users.noreply.github.com>
2025-07-28 13:38:35 +0100

    Fix pos encoding in induction head experiment  (#88)

--- spd/experiments/ih/model.py
+++ spd/experiments/ih/model.py
@@ -49,7 +49,9 @@ class PositionalEncoding(nn.Module):
         super().__init__()
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float32).unsqueeze(1)
-        div_term = 10000.0 ** (torch.arange(0, d_model, 2).float() / d_model)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2, dtype=torch.float32) * (-math.log(10000.0) / d_model)
+        )              
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         self.register_buffer("pe", pe.unsqueeze(0))
