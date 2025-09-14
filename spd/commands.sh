68a06189c86db2e10789f7b79b5cdd0b63bd8077
nhu <nhu@tenant-ac-goodfre-login-0.tenant-ac-goodfre-login.tenant-ac-goodfre.svc.cluster.local>
2025-09-14 07:09:07 +0000

    Sweep snapshot 20250914-070907

--- spd/commands.sh
+++ spd/commands.sh
@@ -1,11 +1,32 @@
 
-#103271
+#103271_1
 spd-run --experiments ss_llama_subset_sans --dp 4
 
-#103267
+#103272
 spd-run --experiments ss_llama_subset_sans_simple --dp 4
 
 
-#103266
+#103273
 spd-run --experiments ss_llama_subset_bal_sans --dp 4
 
+#103274, 103330 (matching lucius hparams)
+spd-run --experiments ss_llama_sans_base --dp 4
+
+
+#103376
+spd-run --experiments ss_llama_subset_bal_sans1 --dp 4
+
+#103377
+spd-run --experiments ss_llama_subset_bal_sans2 --dp 4
+
+#103379
+spd-run --experiments ss_llama_subset_bal_sans3 --dp 4
+
+#103380
+spd-run --experiments ss_llama_subset_bal_sans4 --dp 4
+
+#103381
+spd-run --experiments ss_llama_subset_bal_sans5 --dp 4
+
+#103378
+spd-run --experiments ss_llama_sans_base_lucius --dp 8
