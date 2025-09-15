a1619fa9298f92e910212a53c5b2885b7a7c8e52
Dan Braun <dan.braun@goodfire.ai>
2025-09-15 13:56:37 +0000

    Sweep snapshot 20250915-135636

--- spd/models/component_model.py
+++ spd/models/component_model.py
@@ -21,6 +21,7 @@ from spd.mask_info import ComponentsMaskInfo
 from spd.models.components import (
     Components,
     ComponentsOrModule,
+    EmbeddingComponents,
     GateMLPs,
     GateType,
     LinearComponents,
@@ -259,12 +260,11 @@ class ComponentModel(LoadableModule):
                         bias=module.bias.data if module.bias is not None else None,  # pyright: ignore[reportUnnecessaryComparison]
                     )
                 elif isinstance(module, nn.Embedding):
-                    raise ValueError("Embedding modules currently broken")
-                    # components = EmbeddingComponents(
-                    #     C=C,
-                    #     vocab_size=module.num_embeddings,
-                    #     embedding_dim=module.embedding_dim,
-                    # )
+                    components = EmbeddingComponents(
+                        C=C,
+                        vocab_size=module.num_embeddings,
+                        embedding_dim=module.embedding_dim,
+                    )
                 elif isinstance(module, RadfordConv1D):
                     d_in, d_out = module.weight.shape
                     components = LinearComponents(
