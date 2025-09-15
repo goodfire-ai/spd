b715739dededf6c730d3b933fd932ac039367476
nhu <nhu@tenant-ac-goodfre-login-0.tenant-ac-goodfre-login.tenant-ac-goodfre.svc.cluster.local>
2025-09-15 05:05:33 +0000

    Sweep snapshot 20250915-050533

---
+++ spd/commands.sh
@@ -0,0 +1,11 @@
+#103503
+spd-run --experiments ss_llama_sans_base --dp 4
+
+#103504
+spd-run --experiments ss_llama_subset_sans --dp 4
+
+#103505
+spd-run --experiments ss_llama_sans_bal --dp 4
+
+#103506
+spd-run --experiments ss_llama_subset_bal_sans --dp 4
