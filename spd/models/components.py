4e142c983b655147641c4247f25a69511b9a8e4f
Lucius Bushnaq <Antifrequentist@proton.me>
2025-10-01 10:02:47 +0000

    Sweep snapshot 20251001-100247

--- spd/models/components.py
+++ spd/models/components.py
@@ -102,6 +102,7 @@ class VectorSharedMLPGate(nn.Module):
         for i in range(len(hidden_dims)):
             in_dim = input_dim if i == 0 else hidden_dims[i - 1]
             output_dim = hidden_dims[i]
+            self.layers.append(nn.LayerNorm(in_dim))
             self.layers.append(Linear(in_dim, output_dim, nonlinearity="relu"))
             self.layers.append(nn.GELU())
         final_dim = hidden_dims[-1] if len(hidden_dims) > 0 else input_dim
