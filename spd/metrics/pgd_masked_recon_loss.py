1b23c8953777f980f2f53d79868f1e5de52a4b85
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2025-10-14 16:08:03 +0000

    Sweep snapshot 20251014-160803

--- spd/metrics/pgd_masked_recon_loss.py
+++ spd/metrics/pgd_masked_recon_loss.py
@@ -50,8 +50,8 @@ class PGDReconLoss(Metric):
         self.pgd_config: PGDConfig = pgd_config
         self.output_loss_type: Literal["mse", "kl"] = output_loss_type
         self.use_delta_component: bool = use_delta_component
-        self.sum_loss = torch.tensor(0.0, device=device)
-        self.n_examples = torch.tensor(0, device=device)
+        self.sum_losses = [torch.tensor(0.0, device=device) for _ in range(pgd_config.n_steps)]
+        self.n_exampleses = [torch.tensor(0, device=device) for _ in range(pgd_config.n_steps)]
 
     @override
     def update(
@@ -63,21 +63,33 @@ class PGDReconLoss(Metric):
         weight_deltas: dict[str, Float[Tensor, "d_out d_in"]] | None,
         **_: Any,
     ) -> None:
-        sum_loss, n_examples = pgd_masked_recon_loss_update(
-            model=self.model,
-            ci=ci,
-            weight_deltas=weight_deltas if self.use_delta_component else None,
-            output_loss_type=self.output_loss_type,
-            batch=batch,
-            target_out=target_out,
-            routing="all",
-            pgd_config=self.pgd_config,
-        )
-        self.sum_loss += sum_loss
-        self.n_examples += n_examples
+        for i in range(self.pgd_config.n_steps):
+            sum_loss, n_examples = pgd_masked_recon_loss_update(
+                model=self.model,
+                ci=ci,
+                weight_deltas=weight_deltas if self.use_delta_component else None,
+                output_loss_type=self.output_loss_type,
+                batch=batch,
+                target_out=target_out,
+                routing="all",
+                pgd_config=PGDConfig(
+                    init=self.pgd_config.init,
+                    step_size=self.pgd_config.step_size,
+                    n_steps=i + 1,
+                    mask_scope=self.pgd_config.mask_scope,
+                ),
+            )
+            self.sum_losses[i] += sum_loss
+            self.n_exampleses[i] += n_examples
 
     @override
-    def compute(self) -> Float[Tensor, ""]:
-        sum_loss = all_reduce(self.sum_loss, op=ReduceOp.SUM)
-        n_examples = all_reduce(self.n_examples, op=ReduceOp.SUM)
-        return sum_loss / n_examples
+    def compute(self) -> dict[str, Float[Tensor, ""]]:
+        out: dict[str, Float[Tensor, ""]] = {}
+        for i in range(self.pgd_config.n_steps):
+            sum_loss = all_reduce(self.sum_losses[i], op=ReduceOp.SUM)
+            n_examples = all_reduce(self.n_exampleses[i], op=ReduceOp.SUM)
+            out[f"pgd_loss_{i}_steps"] = sum_loss / n_examples
+        return out
+        # sum_loss = all_reduce(self.sum_loss, op=ReduceOp.SUM)
+        # n_examples = all_reduce(self.n_examples, op=ReduceOp.SUM)
+        # return sum_loss / n_examples
