f318627ce019a75f5bcd4fa1ae0e5d66ddb71275
Lucius Bushnaq <Antifrequentist@proton.me>
2025-10-20 11:15:16 +0000

    Sweep snapshot 20251020-111515

--- spd/run_spd.py
+++ spd/run_spd.py
@@ -24,7 +24,7 @@ from spd.log import logger
 from spd.losses import compute_total_loss
 from spd.metrics import faithfulness_loss
 from spd.metrics.alive_components import AliveComponentsTracker
-from spd.models.component_model import ComponentModel, OutputWithCache
+from spd.models.component_model import ComponentModel, OutputWithCache, SPDRunInfo
 from spd.utils.component_utils import calc_ci_l_zero
 from spd.utils.distributed_utils import (
     avg_metrics_across_ranks,
@@ -136,7 +136,12 @@ def optimize(
         pretrained_model_output_attr=config.pretrained_model_output_attr,
         sigmoid_type=config.sigmoid_type,
     )
-
+    # Load a model from pretrained run
+    # run_info = SPDRunInfo.from_path("goodfire/spd/3rthvni3")
+    # model = ComponentModel.from_run_info(run_info)
+    # config = run_info.config
+    # target_model = model.target_model
+    # target_model.requires_grad_(False)
     if ln_stds is not None:
         # model has ablated layernorms, patch in the fixed std values
         replace_std_values_in_layernorm(model, ln_stds)
