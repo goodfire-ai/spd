75e569366cbf6591e04ba2013df99ace60789773
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2025-11-05 22:05:41 +0000

    wip: Remove obsolete Svelte event type augmentation

--- spd/app/frontend/src/svelte-events.d.ts
+++
@@ -1,8 +0,0 @@
-// Temporary type augmentation for Svelte 5 onclick events
-// Remove this file once Svelte type definitions are updated
-
-declare module 'svelte/elements' {
-    interface HTMLButtonAttributes {
-        onclick?: ((event: MouseEvent) => void) | undefined;
-    }
-}
