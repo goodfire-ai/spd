da6275653d45bd84dd3f682c24cc76daa18452c9
Lucius Bushnaq <Antifrequentist@proton.me>
2026-01-10 10:36:47 +0000

    run id run_20260110_103646

--- spd/run_spd.py
+++ spd/run_spd.py
@@ -281,6 +281,7 @@ def optimize(
                 use_delta_component=config.use_delta_component,
                 n_mask_samples=config.n_mask_samples,
                 output_loss_type=config.output_loss_type,
+                module_info=config.module_info,
             )
             microbatch_total_loss.div_(config.gradient_accumulation_steps).backward()
 
