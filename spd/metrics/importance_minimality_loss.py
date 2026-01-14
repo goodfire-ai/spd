7b8ec3e79eeb296aaad939d79265456d06d1f066
Lucius Bushnaq <Antifrequentist@proton.me>
2026-01-14 18:56:44 +0000

    run id run_20260114_185643

--- spd/metrics/importance_minimality_loss.py
+++ spd/metrics/importance_minimality_loss.py
@@ -103,7 +103,7 @@ def _importance_minimality_loss_compute(
     total_loss = torch.tensor(0.0, device=next(iter(per_component_sums.values())).device)
     for layer_sums in per_component_sums.values():
         per_component_mean = layer_sums / n_examples
-        layer_loss = (per_component_mean + beta * per_component_mean**pnorm_2).sum()
+        layer_loss = (beta * per_component_mean*torch.log2(layer_sums + 1.0)).sum()
         total_loss += layer_loss
     return total_loss
 
