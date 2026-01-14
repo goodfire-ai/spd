27f9e74207eb6be485a8a17007dcdf41af2ca2af
Lucius Bushnaq <Antifrequentist@proton.me>
2026-01-14 11:35:40 +0000

    run id run_20260114_113540

--- spd/metrics/importance_minimality_loss.py
+++ spd/metrics/importance_minimality_loss.py
@@ -104,7 +104,8 @@ def _importance_minimality_loss_compute(
     total_loss = None
     for layer_sums in per_component_sums.values():
         per_component_mean = layer_sums / total_params
-        layer_loss = (per_component_mean**pnorm_2).sum()
+        per_component_entropy = -per_component_mean * torch.log2(per_component_mean+1e-10)- (1-per_component_mean) * torch.log2(1-per_component_mean+1e-10)
+        layer_loss = (per_component_mean+10*per_component_mean*per_component_entropy).sum()
         total_loss = layer_loss if total_loss is None else total_loss + layer_loss
     assert total_loss is not None, "Empty per_component_sums"
     return total_loss
